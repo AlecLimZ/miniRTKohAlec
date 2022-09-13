057bbca4ded0d1de4ba269ba85c02ab22102dc8d
leng-chu <jacklearn@DESKTOP-HI558GB.localdomain>
2022-09-13 20:52:18 +0800

    add basic coding

--- OurMiniRT/main.c
+++ OurMiniRT/main.c
@@ -6,16 +6,45 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:33:51 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/09/13 17:28:48 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/09/13 20:48:43 by leng-chu         ###   ########.fr       */
 /*   Updated: 2021/12/07 11:48:40 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int	main(int argc, char **argv)
+typedef struct s_data
 {
-	(void)argc;
-	(void)argv;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	ft_pixel(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1000, 500, "HELLO");
+	img.img = mlx_new_image(mlx, 300, 300);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	for (int i = 0; i < 300; i++)
+		for (int j = 0; j < 300; j++)
+			ft_pixel(&img, i, j, 0x00FF0000); // pixel positon is on image map
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 100, 0); // image positon is on window map
+	mlx_loop(mlx);
+	(void)mlx_win;
 	return (0);
 }
