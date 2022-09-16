94070b146ac147c154d314b26156e49e2811dc6a
leng-chu <jacklearn@DESKTOP-HI558GB.localdomain>
2022-09-16 13:05:42 +0800

    cleanup some stuffs

--- SANDBOX/SRC/INCLUDES/vec.h
+++ SANDBOX/SRC/INCLUDES/vec.h
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:29:08 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/09/15 18:43:59 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/09/16 13:00:31 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ typedef struct s_vec3
 
 double	ft_squared_len(const t_vec3 *v);
 double	ft_length(const t_vec3 *v);
+void	ft_cvntminus(t_vec3 *v);
+void	ft_plus2vecs(t_vec3 *a, const t_vec3 *b);
+void	ft_multivec(t_vec3 *v, const double b);
+void	ft_dividevec(t_vec3 *v, const double b);
 
 #endif
--- SANDBOX/SRC/vec1.c
+++ SANDBOX/SRC/vec1.c
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:24:00 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/09/15 18:42:48 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/09/16 12:34:27 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,32 @@ double	ft_squared_len(const t_vec3 *v)
 double	ft_length(const t_vec3 *v)
 {
 	return (sqrt(ft_squared_len(v)));
+}
+
+void	ft_cvntminus(t_vec3 *v)
+{
+	v->rgb[0] *= -1;
+	v->rgb[1] *= -1;
+	v->rgb[2] *= -1;
+}
+
+void	ft_plus2vecs(t_vec3 *a, const t_vec3 *b)
+{
+	a->rgb[0] += b->rgb[0];
+	a->rgb[1] += b->rgb[1];
+	a->rgb[2] += b->rgb[2];
+}
+
+void	ft_multivec(t_vec3 *v, const double b)
+{
+	v->rgb[0] *= b;
+	v->rgb[1] *= b;
+	v->rgb[2] *= b;
+}
+
+void	ft_dividevec(t_vec3 *v, const double b)
+{
+	v->rgb[0] /= b;
+	v->rgb[1] /= b;
+	v->rgb[2] /= b;
 }
