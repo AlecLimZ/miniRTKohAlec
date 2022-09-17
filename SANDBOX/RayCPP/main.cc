efceaf373324bace3c0f2a0e153942053bfc624a
Lim Eng chuan <leng-chu@u81z03s04.42kl.edu.my>
2022-09-17 14:50:38 +0800

    done 5th topic in C

--- SANDBOX/RayCPP/main.cc
+++ SANDBOX/RayCPP/main.cc
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 17:43:23 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/09/15 17:52:06 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/09/17 14:25:47 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@
 #include "color.h"
 #include "ray.h"
 
+// hit on the sphere
+bool	hit_sphere(const point3 & center, double radius, const ray & r)
+{
+	vec3	oc = r.getOrigin() - center;
+	double	a = dot(r.getDirection(), r.getDirection());
+	double	b = 2.0 * dot(oc, r.getDirection());
+	double	c = dot(oc, oc) - radius * radius;
+	double	discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
 // color is vec3 class - but role is color
 color ray_color(const ray & r)
 {
@@ -23,9 +34,12 @@ color ray_color(const ray & r)
 	// calling inline vec3 operator/(vec3 v, double t) return 
 	// (1 / t) * v which calling vec3 operator*(double t, const vec3 & v)
 	// return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0);
 	vec3	unit_direction = unit_vector(r.getDirection());
 	double	t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	//return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + (t) * color(0.5, 0.7, 1.0);
 }
 
 int main()
