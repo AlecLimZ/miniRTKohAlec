801b61f84adeb03d3478a6b3b73bbd1e3b6e9b40
leng-chu <jacklearn@DESKTOP-HI558GB.localdomain>
2022-10-05 23:07:08 +0800

    continue to source

--- SANDBOX/SCRATCHPIXEL/cameraray_notes.h
+++ SANDBOX/SCRATCHPIXEL/cameraray_notes.h
@@ -6,7 +6,7 @@
 /*   By: leng-chu <leng-chu@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 19:00:19 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/10/04 13:55:41 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/10/05 19:15:33 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -227,3 +227,193 @@ Dz = (x - width / 2)uz + (height / 2 - y)vz - ((height / 2) / tan (fov * 0.5))wz
 //vector u, v & w is just the camera-to-world matrix's 3 points coordinate
 Vec3f w_p = (-width / 2 ) * u + (height / 2) * v - ((height / 2) / tan(fov_rad * 0.5);
 Vec3f ray_dir = normalize(x * u + y * (-v) + w_p);
+
+// RENDERING SPHERES
+class Object
+{
+	public:
+	virtual bool intersect(const Vec3f & orig, const Vec3f & dir, float & bt) const = 0;
+	virtual ~Object() {}
+	Object() {}
+};
+
+class Sphere: public Object
+{
+	public:
+	bool intersect(const Vec3f & orig, const Vec3f & dir, float & t) const
+	{
+		return false;
+	}
+};
+
+int main(...)
+{
+	Sphere mySphere;
+	...
+		if (mySphere.intersect(orig, dir, t) {
+					// this ray intersect this instance of the class Sphere
+				}
+}
+
+// 2nd pseudo-code
+class Sphere: public Object
+{
+	bool intersect(const Vec3f & orig, const Vec3f & dir, float & t) const
+	{
+		float t0, t1;
+#if 0
+		Vec3f L = center - orig;
+		float tca = L.dotProduct(dir);
+		if (tca < 0) return false;
+		float d2 = L.dotProduct(L) - tca * tca;
+		if (d2 > radius2) return false;
+		float thc = sqrt(radius2 - d2);
+		t0 = tca - thc;
+		t1 = tca + thc;
+#else
+		Vec3f L = orig - center;
+		float a = dir.dotProduct(dir);
+		float b = 2 * dir.dotProduct(L);
+		float c = L.dorProduct(L) - radius2;
+		if (!solveQuadratic(a, b, c, t0, t1)) return false;
+#endif
+		if (t0 > t1) std::swap(t0, t1);
+		if (t0 < 0) {
+			t0 = t1;
+			if (t0 < 0) return false;
+		}
+		t = t0;
+		return true;
+	}
+};
+
+// then compute the intersection of a ray with this geometry to the intersect
+class TriangulatedMesh : public Object
+{
+	public:
+		bool intersect(const Vec3f & orig, const Vec3f & dir, float & t) const
+		{
+			return true;
+		}
+};
+
+// then creating a scene
+int main(int argc, char **argv)
+{
+	// creating the scene (adding objects and lights)
+	std::vector<std::unique_ptr<Object> > objects;
+
+	// generate a scene made of random spheres
+	uint32_t numSpheres = 32;
+	gen.seed(0);
+	for (uint32_t i = 0; i < numSpheres; ++i) {
+		Vec3f randPos((0.5 - dis(gen)) * 10, (0.5 - dis(gen)) * 10, (0.5 + dis(gen) * 10));
+		float randRadius = (0.5 + dis(gen) * 0.5);
+		objects.push_back(std::unique_ptr<Object>(new Sphere(randPos, randRadius)));
+	}
+
+	// setting up options
+	Options options;
+	options.width = 640;
+	options.height = 480;
+	options.fov = 51.52;
+	options.cameraToWorld = Matrix44f(0.945519, 0, -0.325569, 0, -0.179534, 0.834209l, -0.521403, 0, 0.271593, 0.551447, 0.78876, 0, 4.208271, 8.374532, 17.932925, 1);
+
+	// finally, render
+	render(options, objects);
+	return (0);
+}
+
+// then render, make primary rays.
+// origin & directon are transform by camera-to-world
+void render(const Options &options, const std::vector<std::unique_ptr<Objects>> & objects)
+{
+	...
+	float scale = tan(deg2rad(options.fov * 0.5));
+	float imageAspectRatio = options.width / (float)options.height;
+	Vec3f orig;
+	options.cameraToWorld.multVecMatrix(Vec3f(0), orig);
+	for (uint32_t j = 0; j < options.height, ++j) {
+		for (uint32_t i = 0; i < options.width; ++i) {
+			float x = (2 * (i + 0.5) / (float)options.width - 1) * imageAspectRatio * scale;
+			float y = (1 - 2 * (j + 0.5) / (float)options.height) * scale;
+			Vec3f dir;
+			options.cameraToWorld.multDirMatrix(Vec3f(x, y, -1), dir);
+			dir.normalize();
+			*(pix++) = castRay(orig, dir, objects);
+		}
+	}
+	// save to PPM image
+}
+
+// if intersect,return true
+// trace() to keep track of the object withh closest intersection distance
+// as the ray may intersect more than one object.
+
+bool trace(const Vec3f & orig, const Vec3f & dir, const std::vector<std::unique_ptr<Object>> & objects, float & tNear, const Object * & hitObject)
+{
+	tNear = kInfinity;
+	std::vector<std::unique_ptr<object>>::const_iterator iter= "objects.begin();" for="" (;="" !="objects.end(); " ++iter)="" { ="" float="" t="kInfinity;" if="" ((*iter)-="">intersect(orig, dir, t) && t < tNear) {
+		hitObject = iter->getj();
+		tNear = t;
+		}
+	}
+	return (hitObject != nullptr);
+}</std::unique_ptr<object>>::const_iterator>
+
+/**Ray-Plane intersection**/
+bool intersectPlane(const Vec3f & n, const Vec3f & p0, const Vec3f & 10, const Vec3f &l, float & t)
+{
+	// assuming vectors are all normalized
+	float denom = dotProduct(n, 1);
+	if (denom > 1e - 6) {
+		Vec3f p010 = p0 - 10;
+		t = dotProduct(p010, n) / denom;
+		return (t >= 0);
+	}
+	return false;
+}
+
+// intersectDisk
+// if distance is lower or equal to the disk radius, it means
+// the ray intersects the disk
+bool intersectDisk(const Vec3f & n, const Vec3f & p0, const float & radius, const Vec3f & l0, const Vec3 & l)
+{
+	float t = 0;
+	if (intersectPlane(n, p0, l0, l, t)) {
+		Vec3f p = 10 + l * t;
+		Vec3f v = p - p0;
+		float d2 = dot(v, v);
+		return (sqrt(d2) <= radius);
+		// or you can use the following optimisation (and precompute radius ^ 2)
+		// return d2 <= radius2; // where radius2 = radius * radius
+	}
+	return false;
+}
+
+// computing the intersection of a ray with an AABB is
+// y = mx + b
+// m term is called the slope or gradient, responsible for the orientation
+// of the line.
+// b corresponds to the point where the line intersect the y-axis
+// Thr ray can be expressed as: O + Dt
+// which is very similar to the line equation y = mx + b
+// so O replace with b and D with m
+// to represent an axis-aligned bounding volumn, all we need are two points
+// minimum & maximum extend of the box
+class Box3
+{
+	public:
+		Box3(const Vec3f & vmin, const Vec3f & vmax)
+		{
+			bounds[0] = vmin;
+			bounds[1] = vmax;
+		}
+		Vec3f bounds[2];
+};
+
+// lets say line defined by the equation: y = B0x
+// where B0x is the bounds[0].x
+// to find where the ray intersects, we can write:
+// Ox + tDx = B0x, which can solved by reordering the terms:
+// t0x = (B0x - Ox) / Dx
